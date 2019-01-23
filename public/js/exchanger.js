884cfef28ffd6336f7df0c9f99e22a7962d76eb1
adee2210 <adee_2210@hotmail.com>
2019-01-24 00:07:17 +0700

    First upload

---
+++ public/js/exchanger.js
@@ -0,0 +1,63 @@
+class exchanger {
+    constructor(rate){
+        this.rate = rate;
+
+        this.listening();
+    }
+
+    listening(){
+        var obj = this;
+        $('input.from-amount').keyup(function(){
+            var rate = `${obj.rate}`;
+            $('.lbl-from').html($(this).val()+' '+$('select.from-symbol option:selected').text()+' equal to');
+            var conv = (rate*$(this).val()).toFixed(5);
+            $('.lbl-to').html(conv+' '+$('select.to-symbol option:selected').text());
+            $('input.to-amount').val(conv);
+        });
+        $('input.to-amount').keyup(function(){
+            var rate = `${obj.rate}`;
+            $('.lbl-to').html($(this).val()+' '+$('select.to-symbol option:selected').text());
+            var conv = (1/rate*$(this).val()).toFixed(5);
+            $('.lbl-from').html(conv+' '+$('select.from-symbol option:selected').text()+' equal to');
+            $('input.from-amount').val(conv);
+        });
+
+        $('select.from-symbol').change(function(){
+            var symbol = $(this).val();
+            $.post(
+                '/request',
+                {symbol:symbol},
+                function(response){
+                    if(response.rate >= 0){
+                        obj.rate = response.rate;
+                        var conv = (obj.rate*$('input.from-amount').val()).toFixed(5);
+                        $('input.to-amount').val(conv);
+                    }
+                    if(typeof(response.data) == 'object'){
+                        $('select.to-symbol').html('');
+                        $.each(response.data,function(idx,val){
+                            $('select.to-symbol').append('<option>'+val+'</option>');
+                        });
+                    }
+                }, 
+                "json"
+            );
+        });
+
+        $('select.to-symbol').change(function(){
+            var symbol = $(this).val();
+            $.post(
+                '/rate',
+                {fromsymbol:$('select.from-symbol option:selected').text(),tosymbol:symbol},
+                function(response){
+                    if(response.rate >= 0){
+                        obj.rate = response.rate;
+                        var conv = (1/obj.rate*$('input.to-amount').val()).toFixed(5);
+                        $('input.from-amount').val(conv);
+                    }
+                }, 
+                "json"
+            );
+        });
+    }
+}
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,118 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController ;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use OneForge\ForexQuotes\ForexDataClient;
+use Symfony\Component\Cache\Simple\FilesystemCache;
+
+class HomeController extends AbstractController {
+    private $defaultcurrency = 'USD';
+    private $api_key = 'EQSnBJo9GkXJRdzzoWGAjxD2b7RwUtsS';
+    public function index()
+    {
+        try{
+            $cache = new FilesystemCache();
+            if(!$cache->has('symbols')){
+                if(!($symbols = $this->getcurrency()))
+                    throw new Exception('Unknown response format from ForexDataClient');
+            }
+            else
+                $symbols = $cache->get('symbols');
+            
+            $vars = array(
+                'fromsymbol' => array_keys($symbols),
+                'tosymbol' => $symbols[$this->defaultcurrency],
+                'default' => $this->defaultcurrency,
+                'rate' => $this->getrate($this->defaultcurrency,$symbols[$this->defaultcurrency][0])
+            );
+            return $this->render('frontend/index.html.twig', $vars);
+        }
+        catch(Exception $e){
+            return new Response(
+                $e->getMessage()
+            );
+        }
+    }
+
+    public function request(Request $request){
+        try{
+            if($request->isXMLHttpRequest()){
+                $symbols = array();
+                $cache = new FilesystemCache();    
+                if($cache->has('symbols'))
+                    $symbols = $cache->get('symbols');
+                if(!isset($symbols[$request->request->get('symbol')]))
+                    if(!($symbols = $this->getcurrency()))
+                        throw new Exception('Unknown response format from ForexDataClient');
+                    elseif(!isset($symbols[$request->request->get('symbol')]))
+                        throw new Exception('Currency symbol not exists in ForexDataClient');
+                
+                return new JsonResponse(array(
+                    'rate' => $this->getrate($request->request->get('symbol'),$symbols[$request->request->get('symbol')][0]),
+                    'data' => $symbols[$request->request->get('symbol')]
+                ));
+            }
+        }
+        catch(Exception $e){
+            return new Response(
+                $e->getMessage()
+            );
+        }
+    }
+
+    public function rate(Request $request){
+        try{
+            if($request->isXMLHttpRequest()){
+                return new JsonResponse(array(
+                    'rate' => $this->getrate($request->request->get('fromsymbol'),$request->request->get('tosymbol'))
+                ));
+            }
+        }
+        catch(Exception $e){
+            return new Response(
+                $e->getMessage()
+            );
+        }
+    }
+
+    private function getcurrency($symbols){
+        if($symbols = $this->loadcurrency()){
+            $relsymbol = array();
+            if(!empty($symbols))
+                foreach($symbols as $symbol)
+                    if(strlen($symbol)==6)
+                        $relsymbol[substr($symbol,0,3)][] = substr($symbol,3,3);
+            if(!empty($relsymbol)){
+                $cache = new FilesystemCache();
+                $cache->set('symbols', $relsymbol);
+                return $relsymbol;
+            }
+        }
+        else
+            throw new Exception('Could\'t connect to API');
+    }
+
+    private function loadcurrency(){
+        $client = new ForexDataClient($this->api_key);
+        return $client->getSymbols();
+    }
+
+    private function getrate($fromcurr,$tocurr){
+        $exhanges = array();
+        $cache = new FilesystemCache();
+        if($cache->has('exhanges')){
+            $exhanges = $cache->get('exhanges');
+            if(isset($exhanges["{$fromcurr}{$tocurr}"]))
+                return $exhanges["{$fromcurr}{$tocurr}"];
+        }
+        $client = new ForexDataClient($this->api_key);
+        if($rate = $client->convert($fromcurr, $tocurr, 1)){
+            $exhanges["{$fromcurr}{$tocurr}"] = $rate['value'];
+            $cache->set('exhanges', $exhanges);
+            return $rate['value'];
+        }
+    }
+}
