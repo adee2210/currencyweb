6d2b192b22947ffcbac89bd069ee8295c4c8885f
adee2210 <adee_2210@hotmail.com>
2019-01-25 21:48:27 +0700

    prevent from enter non numeric

--- public/js/exchanger.js
+++ public/js/exchanger.js
@@ -8,18 +8,22 @@ class exchanger {
     listening(){
         var obj = this;
         $('input.from-amount').keyup(function(){
-            var rate = `${obj.rate}`;
-            $('.lbl-from').html($(this).val()+' '+$('select.from-symbol option:selected').text()+' equal to');
-            var conv = (rate*$(this).val()).toFixed(5);
-            $('.lbl-to').html(conv+' '+$('select.to-symbol option:selected').text());
-            $('input.to-amount').val(conv);
+            if(obj.decimal($(this))){
+                var rate = `${obj.rate}`;
+                $('.lbl-from').html($(this).val()+' '+$('select.from-symbol option:selected').text()+' equal to');
+                var conv = (rate*$(this).val()).toFixed(5);
+                $('.lbl-to').html(conv+' '+$('select.to-symbol option:selected').text());
+                $('input.to-amount').val(conv);
+            }
         });
         $('input.to-amount').keyup(function(){
-            var rate = `${obj.rate}`;
-            $('.lbl-to').html($(this).val()+' '+$('select.to-symbol option:selected').text());
-            var conv = (1/rate*$(this).val()).toFixed(5);
-            $('.lbl-from').html(conv+' '+$('select.from-symbol option:selected').text()+' equal to');
-            $('input.from-amount').val(conv);
+            if(obj.decimal($(this))){
+                var rate = `${obj.rate}`;
+                $('.lbl-to').html($(this).val()+' '+$('select.to-symbol option:selected').text());
+                var conv = (1/rate*$(this).val()).toFixed(5);
+                $('.lbl-from').html(conv+' '+$('select.from-symbol option:selected').text()+' equal to');
+                $('input.from-amount').val(conv);
+            }
         });
 
         $('select.from-symbol').change(function(){
@@ -63,5 +67,18 @@ class exchanger {
                 "json"
             );
         });
+    }
+
+    decimal(obj){
+        var val = obj.val().replace(/[^0-9\.]/,'');
+        var dot = val.split(".");
+        if(dot.length>2)
+            val = dot[0]+"."+dot[1];
+        if(val.length>1)
+            if(val[0]==0)
+                val = val.slice(1);
+        if(val.length==0)
+            val = 0;
+        return obj.val(val);
     }
 }
